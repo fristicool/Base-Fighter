0d4975fed8d3a6b083a07a7ebb32947db46bf57a
fristicool <74139390+fristicool@users.noreply.github.com>
2021-08-06 20:46:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const serverless = require('serverless-http');
 
 const { mongoose } = require('./db.js');
 var baseController = require('./controllers/baseController.js');
+var troopController = require('./controllers/troopController.js');
 
 var app = express();
 app.use(bodyParser.json());
@@ -14,5 +15,6 @@ app.listen(process.env.PORT || 3002, () => console.log('Server stbed at port : 3
 
 
 app.use('/', baseController);
+app.use('/troop', troopController);
 
 module.exports.handler = serverless(app)
